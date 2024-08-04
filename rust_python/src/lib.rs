87d141cde7e9e0b49e82f705c19260bfc4961c18
Evan Febrianto <van.evanfebrianto@gmail.com>
2024-08-04 23:40:06 +0800

    chore: remove unnecesasry comments

--- rust_python/src/lib.rs
+++ rust_python/src/lib.rs
@@ -1,12 +1,11 @@
 use pyo3::prelude::*;
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn rust_python(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m)]
-    fn sum_of_squares_rust(n: usize) -> PyResult<u128> {  // Changed usize to u128
-        let mut total: u128 = 0;  // Changed total type to u128
-        for i in 0..n as u128 {  // Cast loop variable to u128
+    fn sum_of_squares_rust(n: usize) -> PyResult<u128> {
+        let mut total: u128 = 0;
+        for i in 0..n as u128 {
             total = total.checked_add(i * i).ok_or_else(|| {
                 PyErr::new::<pyo3::exceptions::PyOverflowError, _>("Overflow occurred")
             })?;
